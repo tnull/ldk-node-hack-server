695344afdad005fa19c3ba77e203842d244bce70
SaidAlaoui <121897579+saidalaoui-sq@users.noreply.github.com>
2024-06-25 19:57:22 -0400

    formatting

--- common/build.rs
+++ common/build.rs
@@ -1,7 +1,7 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::configure()
-        .protoc_arg("--experimental_allow_proto3_optional")
-        .protoc_arg("--proto_path=../src/proto")
-        .compile(&["ldk_server_hack.proto"], &["*"])?;
-    Ok(())
+	tonic_build::configure()
+		.protoc_arg("--experimental_allow_proto3_optional")
+		.protoc_arg("--proto_path=../src/proto")
+		.compile(&["ldk_server_hack.proto"], &["*"])?;
+	Ok(())
 }

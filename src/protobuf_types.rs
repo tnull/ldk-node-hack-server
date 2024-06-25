eca2cfc757278d8a91d844fb26d3fc72c86f2ea3
G8XSU <3442979+G8XSU@users.noreply.github.com>
2024-06-25 12:21:48 -0400

    Add ListChannels Api request/response structs

--- src/protobuf_types.rs
+++ src/protobuf_types.rs
@@ -123,3 +123,133 @@ pub struct PaymentId {
 	#[prost(bytes = "vec", tag = "1")]
 	pub data: ::prost::alloc::vec::Vec<u8>,
 }
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct ListChannelsRequest {}
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct ListChannelsResponse {
+	#[prost(message, repeated, tag = "1")]
+	pub channels: ::prost::alloc::vec::Vec<Channel>,
+}
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct Channel {
+	/// The channel ID (prior to funding transaction generation, this is a random 32-byte
+	/// identifier, afterwards this is the transaction ID of the funding transaction XOR the
+	/// funding transaction output).
+	///
+	/// Note that this means this value is *not* persistent - it can change once during the
+	/// lifetime of the channel.
+	#[prost(string, tag = "1")]
+	pub channel_id: ::prost::alloc::string::String,
+	/// The node ID of our the channel's remote counterparty.
+	#[prost(string, tag = "2")]
+	pub counterparty_node_id: ::prost::alloc::string::String,
+	/// The channel's funding transaction output, if we've negotiated the funding transaction with
+	/// our counterparty already.
+	#[prost(message, optional, tag = "3")]
+	pub funding_txo: ::core::option::Option<Outpoint>,
+	/// The value, in satoshis, of this channel as it appears in the funding output.
+	#[prost(uint64, tag = "4")]
+	pub channel_value_sats: u64,
+	/// The currently negotiated fee rate denominated in satoshi per 1000 weight units,
+	/// which is applied to commitment and HTLC transactions.
+	#[prost(uint32, tag = "5")]
+	pub feerate_sat_per_1000_weight: u32,
+	/// The available outbound capacity for sending HTLCs to the remote peer.
+	///
+	/// The amount does not include any pending HTLCs which are not yet resolved (and, thus, whose
+	/// balance is not available for inclusion in new outbound HTLCs). This further does not include
+	/// any pending outgoing HTLCs which are awaiting some other resolution to be sent.
+	#[prost(uint64, tag = "6")]
+	pub outbound_capacity_msat: u64,
+	/// The available outbound capacity for sending HTLCs to the remote peer.
+	///
+	/// The amount does not include any pending HTLCs which are not yet resolved
+	/// (and, thus, whose balance is not available for inclusion in new inbound HTLCs). This further
+	/// does not include any pending outgoing HTLCs which are awaiting some other resolution to be
+	/// sent.
+	#[prost(uint64, tag = "7")]
+	pub inbound_capacity_msat: u64,
+	/// The number of required confirmations on the funding transactions before the funding is
+	/// considered "locked". The amount is selected by the channel fundee.
+	///
+	/// The value will be `None` for outbound channels until the counterparty accepts the channel.
+	#[prost(uint32, optional, tag = "8")]
+	pub confirmations_required: ::core::option::Option<u32>,
+	/// The current number of confirmations on the funding transaction.
+	#[prost(uint32, optional, tag = "9")]
+	pub confirmations: ::core::option::Option<u32>,
+	/// Is `true` if the channel was initiated (and therefore funded) by us.
+	#[prost(bool, tag = "10")]
+	pub is_outbound: bool,
+	/// Is `true` if both parties have exchanged `channel_ready` messages, and the channel is
+	/// not currently being shut down. Both parties exchange `channel_ready` messages upon
+	/// independently verifying that the required confirmations count provided by
+	/// `confirmations_required` has been reached.
+	#[prost(bool, tag = "11")]
+	pub is_channel_ready: bool,
+	/// Is `true` if the channel (a) `channel_ready` messages have been exchanged, (b) the
+	/// peer is connected, and (c) the channel is not currently negotiating shutdown.
+	///
+	/// This is a strict superset of `is_channel_ready`.
+	#[prost(bool, tag = "12")]
+	pub is_usable: bool,
+	/// Is `true` if this channel is (or will be) publicly-announced
+	#[prost(bool, tag = "13")]
+	pub is_public: bool,
+	/// The difference in the CLTV value between incoming HTLCs and an outbound HTLC forwarded over
+	/// the channel.
+	#[prost(uint32, optional, tag = "14")]
+	pub cltv_expiry_delta: ::core::option::Option<u32>,
+	/// The smallest value HTLC (in msat) the remote peer will accept, for this channel.
+	///
+	/// This field is only `None` before we have received either the `OpenChannel` or
+	/// `AcceptChannel` message from the remote peer.
+	#[prost(uint64, optional, tag = "15")]
+	pub counterparty_outbound_htlc_minimum_msat: ::core::option::Option<u64>,
+	/// The largest value HTLC (in msat) the remote peer currently will accept, for this channel.
+	#[prost(uint64, optional, tag = "16")]
+	pub counterparty_outbound_htlc_maximum_msat: ::core::option::Option<u64>,
+	/// The available outbound capacity for sending a single HTLC to the remote peer. This is
+	/// similar to `outbound_capacity_msat` but it may be further restricted by
+	/// the current state and per-HTLC limit(s). This is intended for use when routing, allowing us
+	/// to use a limit as close as possible to the HTLC limit we can currently send.
+	#[prost(uint64, tag = "17")]
+	pub next_outbound_htlc_limit_msat: u64,
+	/// The minimum value for sending a single HTLC to the remote peer. This is the equivalent of
+	/// `next_outbound_htlc_limit_msat` but represents a lower-bound, rather than
+	/// an upper-bound. This is intended for use when routing, allowing us to ensure we pick a
+	/// route which is valid.
+	#[prost(uint64, tag = "18")]
+	pub next_outbound_htlc_minimum_msat: u64,
+	/// The number of blocks (after our commitment transaction confirms) that we will need to wait
+	/// until we can claim our funds after we force-close the channel. During this time our
+	/// counterparty is allowed to punish us if we broadcasted a stale state. If our counterparty
+	/// force-closes the channel and broadcasts a commitment transaction we do not have to wait any
+	/// time to claim our non-HTLC-encumbered funds.
+	///
+	/// This value will be `None` for outbound channels until the counterparty accepts the channel.
+	#[prost(uint32, optional, tag = "19")]
+	pub force_close_spend_delay: ::core::option::Option<u32>,
+	/// Amount (in millionths of a satoshi) charged per satoshi for payments forwarded outbound
+	/// over the channel.
+	#[prost(uint32, tag = "20")]
+	pub forwarding_fee_proportional_millionths: u32,
+	/// Amount (in milli-satoshi) charged for payments forwarded outbound over the channel, in
+	/// excess of \[`forwarding_fee_proportional_millionths`\].
+	#[prost(uint32, tag = "21")]
+	pub forwarding_fee_base_msat: u32,
+}
+/// Represents to transaction output.
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct Outpoint {
+	/// The referenced transaction's txid.
+	#[prost(string, tag = "1")]
+	pub txid: ::prost::alloc::string::String,
+	/// The index of the referenced output in its transaction's vout.
+	#[prost(uint32, tag = "2")]
+	pub vout: u32,
+}

3df0631091961efd4f355a6f90c8ddbf6613aa57
Austin Mackillop <amackillop@tbd.email>
2024-06-26 10:23:26 -0700

    Generate protos on build

--- protos/build.rs
+++ protos/build.rs
@@ -1,16 +1,11 @@
-#[cfg(genproto)]
 extern crate prost_build;
 
-#[cfg(genproto)]
 use std::{env, fs, path::Path};
 
-/// To generate updated proto objects, run `RUSTFLAGS="--cfg genproto" cargo build`
 fn main() {
-	#[cfg(genproto)]
 	generate_protos();
 }
 
-#[cfg(genproto)]
 fn generate_protos() {
 	prost_build::compile_protos(&["src/proto/ldk_server_hack.proto"], &["src/"])
 		.expect("protobuf compilation failed");
